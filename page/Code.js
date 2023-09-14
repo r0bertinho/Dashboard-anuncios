cac3ebc7d6f2cd43c63d976eed7ed279f01811d5
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-14 18:00:24 -0300

    Ian

---
+++ page/Code.js
@@ -0,0 +1,16 @@
+// This is a JavaScript comment
+
+// Declare a variable and assign a value
+var greeting = "Hello, World!";
+
+// Display the value in the console
+console.log(greeting);
+
+// Define a function
+function sayHello(name) {
+    return "Hello, " + name + "!";
+}
+
+// Call the function and display the result
+var message = sayHello("Alice");
+console.log(message);
