0529e84a16bae79a6bb7458a9225d6e95152f711
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2024-05-23 08:14:22 -0300

    add js

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import 'main.ts';
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const filterBtn = document.getElementById('filter-btn');
+const base = document.getElementById("prod-template");
+const prodList = document.getElementsByClassName("prodtable");
+const leftBtn = document.getElementById('left-btn');
+const rightBtn = document.getElementById('right-btn');
+const perfilBtn = document.getElementById('perfil-image');
+searchBtn.addEventListener('click', () => operation.search());
+rightBtn.addEventListener('click', () => operation.passslide('right'));
+leftBtn.addEventListener('click', () => operation.passslide('left'));
+filterBtn.addEventListener('click', () => subWindow.openfiltertab());
+perfilBtn.addEventListener('click', () => subWindow.openPerfilSubpage());
