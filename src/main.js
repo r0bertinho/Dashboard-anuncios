b010c6017d6c29ea11af6b9c6a480c7e00dcdd17
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2024-06-06 07:58:20 -0300

    correct files // delete 'src/js' folder

--- src/main.js
+++ src/main.js
@@ -1,6 +1,67 @@
-import { LoginOperation } from './js/login.js';
-import { SearchOperations } from './js/search.js';
-import { SubWindow } from './js/subwin.js';
+class SubWindow {
+  #isOpen;
+
+  constructor() {
+    this.#isOpen = false;
+  }
+
+  openfiltertab() {
+    const sideMenu = document.querySelector('.side-menu');
+    this.#isOpen = !this.#isOpen;
+    sideMenu.style.display = this.#isOpen ? 'block' : 'none';
+
+    if (this.#isOpen) {
+      sideMenu.style.width = '0%';
+      setTimeout(() => {
+        sideMenu.style.transition = 'width 0.5s';
+        sideMenu.style.width = '25%';
+      }, 10);
+    } else {
+
+    }
+  }
+
+  openPerfilSubpage() {
+    const subpage = document.querySelector('.subpage-perfil');
+    subpage.style.display = 'block';
+  }
+}
+
+class SearchOperations {
+  constructor() {
+    // implementation
+  }
+
+  search(query) {
+    // implementation
+  }
+
+  filterElements(filterTag) {
+    // implementation
+  }
+
+  passslide(direction) {
+    // implementation
+  }
+
+  createByTemplate(tmplt) {
+    // implementation
+  }
+}
+
+class NetOperations {
+  constructor() {
+
+  }
+
+  SendSignal() {
+
+  }
+
+  GetSignal() {
+
+  }
+}
 
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
@@ -12,6 +73,7 @@ const rightBtn = document.getElementById('right-btn');
 const perfilBtn = document.getElementById('perfil-btn');
 
 let searchOperations = new SearchOperations();
+let NetOperations = new NetOperations();
 let subWindow = new SubWindow();
 
 searchBtn.addEventListener('click', () => searchOperations.search(searchInput.value));
