3b38297995f44f7c045c5801abff50d6f1877c54
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2024-06-06 07:53:46 -0300

    delete class 'elementID'

--- src/main.js
+++ src/main.js
@@ -2,33 +2,20 @@ import { LoginOperation } from './js/login.js';
 import { SearchOperations } from './js/search.js';
 import { SubWindow } from './js/subwin.js';
 
-class ElementID {
-  constructor() {
-    let searchInput = document.getElementById('search-input');
-    let searchBtn = document.getElementById('search-btn');
-    let filterBtn = document.getElementById('filter-btn');
-    let base = document.getElementById("prod-template");
-    let prodList = document.getElementsByClassName("prodtable");
-    let leftBtn = document.getElementById('left-btn');
-    let rightBtn = document.getElementById('right-btn');
-    let perfilBtn = document.getElementById('perfil-btn');
-    this.searchInput = searchInput;
-    this.searchBtn = searchBtn;
-    this.filterBtn = filterBtn;
-    this.base = base;
-    this.prodList = prodList;
-    this.leftBtn = leftBtn;
-    this.rightBtn = rightBtn;
-    this.perfilBtn = perfilBtn;
-  }
-}
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const filterBtn = document.getElementById('filter-btn');
+const base = document.getElementById("prod-template");
+const prodList = document.getElementsByClassName("prodtable");
+const leftBtn = document.getElementById('left-btn');
+const rightBtn = document.getElementById('right-btn');
+const perfilBtn = document.getElementById('perfil-btn');
 
-let elementID = new ElementID();
 let searchOperations = new SearchOperations();
 let subWindow = new SubWindow();
 
-elementID.searchBtn.addEventListener('click', () => searchOperations.search(elementID.searchInput.value));
-elementID.rightBtn.addEventListener('click', () => searchOperations.passslide('right'));
-elementID.leftBtn.addEventListener('click', () => searchOperations.passslide('left'));
-elementID.filterBtn.addEventListener('click', () => subWindow.openfiltertab());
-elementID.perfilBtn.addEventListener('click', () => subWindow.openPerfilSubpage());
+searchBtn.addEventListener('click', () => searchOperations.search(searchInput.value));
+rightBtn.addEventListener('click', () => searchOperations.passslide('right'));
+leftBtn.addEventListener('click', () => searchOperations.passslide('left'));
+filterBtn.addEventListener('click', () => subWindow.openfiltertab());
+perfilBtn.addEventListener('click', () => subWindow.openPerfilSubpage());
